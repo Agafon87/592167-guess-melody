40861bc3433b3dea511e1c135ad2dd023d078710
Алексей <Agafon@MacBook-Pro-Aleksej.local>
2018-12-16 17:02:26 +0330

    Промежуточное выполнение module7-task2

--- js/util.test.js
+++ js/util.test.js
@@ -2,52 +2,21 @@ import {assert} from 'chai';
 import {getResultText} from './util';
 
 
-// const playerScored10Points = [
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30}
-// ];
-// const playerLost = [
-//   {correct: false, time: 30},
-//   {correct: false, time: 30},
-//   {correct: false, time: 30}
-// ];
-// const playerScoredMore10Points = [
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 20},
-//   {correct: true, time: 10},
-//   {correct: true, time: 14},
-//   {correct: true, time: 34},
-//   {correct: true, time: 12},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30},
-//   {correct: true, time: 30}
-// ];
-
-// const timeLost = {
-//   scoredPoints: 10,
-//   notes: 2,
-//   timeLeft: 0
-// };
-// const notesLost = {
-//   scoredPoints: 10,
-//   notes: 0,
-//   timeLeft: 10
-// };
-
-// const normalGame = {
-//   scoredPoints: 13,
-//   notes: 2,
-//   timeLeft: 10
-// };
+const anotherStat = [
+  {
+    "answers": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    "time": 25,
+    "mistakes": 0,
+    "date": 1544944725652
+  },
+  {"answers": [2, 2, 2, 2, 2, 2, -2, 2, 2, 2], "time": 57, "mistakes": 0, "date": 1544945955032},
+  {"answers": [2, 1, 1, 2, 2, 2, 2, 1, 2, 2], "time": 46, "mistakes": 0, "date": 1544946093287},
+  {"answers": [1, 2, 1, 2, -2, 2, 2, 2, 1, 2], "time": 45, "mistakes": 0, "date": 1544946275428},
+  {"answers": [1, 2, 1, 2, -2, 2, 2, -2, 2, 2], "time": 40, "mistakes": 0, "date": 1544946827808},
+  {"answers": [1, 2, 1, 2, -2, 2, 1, 2, 2, 2], "time": 63, "mistakes": 0, "date": 1544947022169},
+  {"answers": [1, 2, 2, 2, -2, 2, 2, 1, 2, 2], "time": 37, "mistakes": 0, "date": 1544947116745},
+  {"answers": [2, 2, 1, 2, 1, 2, 1, 2, -2, 2], "time": 26, "mistakes": 0, "date": 1544947205537}
+];
 
 const gameStat = {
   answers: [2, 1, 2, 1, -2, -2, 1, 1, 1, 1],
@@ -55,24 +24,13 @@ const gameStat = {
   time: 154
 };
 
-const resultAnotherPersonsLocal = [4, 5, 8, 12, 14];
+// const resultAnotherPersonsLocal = [4, 5, 8, 12, 14];
 
-
-// describe(`Проверка функции подсчета очков`, () => {
-//   it(`Должна вернуть 10, если есть ответы на все вопросы и они не быстрые`, () => {
-//     assert.equal(getPointsScored(playerScored10Points), 10);
-//   });
-//   it(`Должна вернуть -1, если игрок не ответил на все 10 вопросов`, () => {
-//     assert.equal(getPointsScored(playerLost), -1);
-//   });
-//   it(`Должна вернуть больше 10, если игрок хороший меломан`, () => {
-//     assert(getPointsScored(playerScoredMore10Points) > 10);
-//   });
-// });
 
 describe(`Проверка функции вывода места игрока в статистике`, () => {
   it(`Проверка, что правильно вычисляется процент в статистике`, () => {
-    assert.equal(getResultText(gameStat, resultAnotherPersonsLocal), `Вы заняли 4 место из 6 игроков. Это лучше, чем у 33% игроков`);
+    assert.equal(getResultText(gameStat, anotherStat), `Вы заняли 9 место из 9 игроков. Это лучше, чем у 0% игроков`);
+    // assert.deepEqual(getResultText(gameStat, anotherStat), [20, 20, 20, 20, 20, 20, 20, 20, 6]);
   });
   // it(`Проверка, что закончились все жизни`, () => {
   //   assert.equal(getResultText(resultAnotherPersonsLocal, notesLost), `У вас закончились все попытки. Ничего, повезёт в следующий раз!`);
